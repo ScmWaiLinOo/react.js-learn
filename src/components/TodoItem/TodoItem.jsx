fdcc97e41a3d72595568bd0ebe4ce44225ee2ce5
WaiWaiLinOo <wailinoo.august@gmail.com>
2022-10-24 21:14:16 +0630

    second commit

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,3 +1,38 @@
-import React from "react";
-const TodoItem = props => <div>{props.title}</div>;
+import React, { useState, Fragment, useEffect } from "react";
+import classes from "./TodoItem.module.css";
+
+const TodoItem = ({ title, id, deleteTodo, updateTodo }) => {
+  const [editTitle, setEditTitle] = useState(title);
+  const [show, setshow] = useState(false);
+  let element = title;
+  useEffect(() => console.log("mounted and updated"), []);
+  const handleUpdate = () => {
+    setshow(false);
+    updateTodo({ id, title: editTitle });
+  };
+  if (show) {
+    element = (
+      <React.Fragment>
+        <input
+          type="text"
+          name="editTitle"
+          value={editTitle}
+          onChange={(e) => setEditTitle(e.target.value)}
+        />
+        <button onClick={handleUpdate}>Update</button>
+      </React.Fragment>
+    );
+  }
+  return (
+    <li className={classes.todoitem}>
+      <span>{element}</span>
+      <button onClick={deleteTodo.bind(this, id)} className={classes.crossX}>
+        X
+      </button>
+      <button style={{ float: "right" }} onClick={(e) => setshow(!show)}>
+        Edit
+      </button>
+    </li>
+  );
+};
 export default TodoItem;
--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -1,10 +1,82 @@
-import React from 'react';
-import TodoItem from '../TodoItem/TodoItem';
-const Todos= props =>(
-  <div>
-    <h1>{props.title}</h1>
-    <TodoItem title="learn react.js"/>
-    <TodoItem title="learn Thazin Moe"/>
-  </div>
-);
+import React, { Component } from "react";
+import TodoItem from "../TodoItem/TodoItem";
+class Todos extends Component {
+  state = {
+    // todos: [
+    //   {
+    //     id: 1,
+    //     title: "learn react",
+    //   },
+    //   {
+    //     id: 2,
+    //     title: "practice react",
+    //   },
+    // ],
+    todos:[],
+    todoTitle: "",
+  };
+  componentDidMount() { //first run
+    console.log('componenetDidMount');
+    fetch("https://jsonplaceholder.typicode.com/todos").then(res =>
+      res.json()).then(data => this.setState({todos:data}));
+  }
+  componentDidUpdate(){
+    //if data change
+    console.log('update');
+  }
+  componentWillUnmount(){
+    //subscribe unsubscribe true or false
+    console.log('destory');
+  }
+  handleChange = (e) => {
+    this.setState({
+      todoTitle: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    const todos = [...this.state.todos];
+    const id = todos.length ? todos[todos.length - 1].id + 1 : 1;
+    alert("A name was submitted: " + this.state.todoTitle);
+    todos.push({ id, title: this.state.todoTitle });
+    this.setState({ todos });
+  };
+  deleteTodo = (id) => {
+    const todos = [...this.state.todos];
+    // const index = todos.findIndex(todo =>todo.id === id);
+    this.setState({ todos: todos.filter((todo) => todo.id !== id) });
+  };
+  updateTodo = (data) => {
+    const todos = [...this.state.todos];
+    const index = todos.findIndex((todo) => todo.id === data.id);
+    todos[index].title = data.title;
+    this.setState(todos);
+  };
+  render = () => {
+    console.log('render');
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <input
+          style={{ marginLeft: "4rem" }}
+          type="text"
+          name="title"
+          value={this.state.todoTitle}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.handleSubmit}>Save</button>
+        <ul>
+          {this.state.todos.map((todo) => (
+            <TodoItem
+              title={todo.title}
+              key={todo.id}
+              deleteTodo={this.deleteTodo}
+              id={todo.id}
+              updateTodo={this.updateTodo}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  };
+}
 export default Todos;
